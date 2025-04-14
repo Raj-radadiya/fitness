e5c14998576ab999197808395085551e20784ab0
Raj-radadiya <radadiyaraj45@gmail.com>
2025-04-14 09:51:17 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './layout/header'
+import Rendering from './rendering'
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <Rendering/>
+    </>
+  )
+}
+
+export default App
